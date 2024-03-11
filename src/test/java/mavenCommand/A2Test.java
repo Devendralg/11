8448d23deed5b49072442c66c0dfff3f14e44787
Admin <Admin@Windows11>
2024-03-11 19:14:28 +0530

    Four test cases ready

---
+++ src/test/java/mavenCommand/A2Test.java
@@ -0,0 +1,18 @@
+package mavenCommand;
+
+import org.testng.annotations.Test;
+
+public class A2Test {
+	
+	@Test
+	public void atest()
+	{
+		System.out.println("A2.atest()");
+	}
+	@Test
+	public void btest()
+	{
+		System.out.println("b2.atest()");
+	}
+
+}
---
+++ src/test/java/mavenCommand/ATest.java
@@ -0,0 +1,18 @@
+package mavenCommand;
+
+import org.testng.annotations.Test;
+
+public class ATest {
+	
+	@Test
+	public void atest()
+	{
+		System.out.println("A.atest()");
+	}
+	@Test
+	public void btest()
+	{
+		System.out.println("b.atest()");
+	}
+
+}
